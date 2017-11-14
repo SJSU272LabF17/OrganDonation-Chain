2c6db65a952f99ecf594ea28bc773031e610ca5a
Maulik Bhatt <bhattmaulik1991>
2017-11-13 18:21:25 -0800

    deleted unwanted folders

--- blockChainUI/OrganChain/src/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyparser from 'body-parser';
-import routes from './src/routes/OrganChainRoutes';
-const app = express();
-const PORT = 7000;
