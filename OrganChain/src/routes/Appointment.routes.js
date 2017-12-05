4d64305d2ebdb9429b07f4eb3ef08a4554ffaad0
Chirag Arora <samkarora@gmail.com>
2017-12-04 20:15:12 -0800

    added rest endpoint for appointments by donorId

--- OrganChain/src/routes/Appointment.routes.js
+++ OrganChain/src/routes/Appointment.routes.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
     app.post('/appointment/donor', appointment.create);
     app.post('/appointment/unos', appointment.createUnosAppointment);
     app.get('/appointment/testing', appointment.scheduledTestingAppts);
+    app.get('/appointment/testing/:donorId', appointment.getAllAppts);
     app.get('/appointment/transplant', appointment.scheduledTransplantAppts);
     app.post('/appointment/complete', appointment.completeTransplant);
 };
