9736175ae38b7fc82c2664b377efe5fca97e7ff9
Chirag Arora <samkarora@gmail.com>
2017-12-10 16:26:32 -0800

    fixed the apis

--- OrganChain/src/controllers/RecipientController.js
+++ OrganChain/src/controllers/RecipientController.js
@@ -8,40 +8,47 @@ exports.create = function(req, res) {
     if(!req.body.name) {
         return res.status(400).send({message: "Recipient name can not be empty"});
     }
-    var recipientInfo = new Recipient({name: req.body.name, age: req.body.age, organ: req.body.organ,
-        hospital: req.body.hospital, email: req.body.email, address: req.body.address, phone: req.body.phone,
-        status: req.body.status, testInfo: req.body.testInfo});
-    recipientInfo.save().then(function(recipient) {
-        return recipient;
-    }).then(recipient => {
-        var recipientCC = {
-            "$class": "org.organchain.Recipient",
-            "recipientId": recipient._id.toString(),
-            "name": recipient.name,
-            "age": recipient.age,
-            "hospital": recipient.hospital.toString(),
-            "email": recipient.email,
-            "organName": recipient.organ,
-            "address": JSON.stringify(recipient.address)
-        };
-
-        var options = {
-            url : blockchain,
-            headers : config.headers,
-            body: JSON.stringify(recipientCC)
-        };
-
-        return request.post(options).then(response => {
-            let json = JSON.parse(response);
-            res.send(recipient);
-        }).catch(err => {
-            console.log("error in saving recipient CC: " + err);
-            throw err;
+    var sql = Recipient.find().where('name').equals(req.body.name)
+        .exec(function(err, recs) {
+            if (err) {
+                var recipientInfo = new Recipient({name: req.body.name, age: req.body.age, organ: req.body.organ,
+                    hospital: req.body.hospital, email: req.body.email, address: req.body.address, phone: req.body.phone,
+                    status: req.body.status, testInfo: req.body.testInfo});
+                recipientInfo.save().then(function(recipient) {
+                    return recipient;
+                }).then(recipient => {
+                    var recipientCC = {
+                        "$class": "org.organchain.Recipient",
+                        "recipientId": recipient._id.toString(),
+                        "name": recipient.name,
+                        "age": recipient.age,
+                        "hospital": recipient.hospital.toString(),
+                        "email": recipient.email,
+                        "organName": recipient.organ,
+                        "address": JSON.stringify(recipient.address)
+                    };
+            
+                    var options = {
+                        url : blockchain,
+                        headers : config.headers,
+                        body: JSON.stringify(recipientCC)
+                    };
+            
+                    return request.post(options).then(response => {
+                        let json = JSON.parse(response);
+                        res.send(recipient);
+                    }).catch(err => {
+                        console.log("error in saving recipient CC: " + err);
+                        throw err;
+                    });
+                }).catch(function(err) {
+                    console.log(err);
+                    res.status(500).send({message: "Some error occurred while creating the RecipientInfo. " + err});
+                });
+            } else {
+                res.status(400).send({message: "already registered recipient"});
+            }
         });
-    }).catch(function(err) {
-        console.log(err);
-        res.status(500).send({message: "Some error occurred while creating the RecipientInfo. " + err});
-    });
 };
 
 exports.findAll = function(req, res) {
--- OrganChain/src/routes/Appointment.routes.js
+++ OrganChain/src/routes/Appointment.routes.js
@@ -2,8 +2,8 @@ module.exports = function(app) {
     var appointment = require('../controllers/AppointmentController.js');
     app.post('/appointment/donor', appointment.create);
     app.post('/appointment/unos', appointment.createUnosAppointment);
-    app.get('/appointment/testing', appointment.scheduledTestingAppts);
+    app.get('/appointment/testing/hospital/:hospitalId', appointment.scheduledTestingAppts);
     app.get('/appointment/testing/:donorId', appointment.getAllAppts);
-    app.get('/appointment/transplant', appointment.scheduledTransplantAppts);
+    app.get('/appointment/transplant/hospital/:hospitalId', appointment.scheduledTransplantAppts);
     app.post('/appointment/complete', appointment.completeTransplant);
 };
