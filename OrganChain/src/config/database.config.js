f67fcc6baefe143b0b023a66d8957ce7d9d23720
Maulik Bhatt <bhattmaulik1991@gmail.com>
2017-12-10 16:34:12 -0800

    fixes

--- OrganChain/src/config/database.config.js
+++ OrganChain/src/config/database.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    url: 'mongodb://10.3.250.144:27017/organChain',
-    //url: 'mongodb://localhost:27017/organChain',
+    //url: 'mongodb://10.3.250.144:27017/organChain',
+    url: 'mongodb://localhost:27017/organChain',
     blockchain: 'http://ec2-52-53-180-100.us-west-1.compute.amazonaws.com:3000/api/',
     headers: {
         'Content-Type': 'application/json',
